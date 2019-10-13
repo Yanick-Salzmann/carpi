ac2b70833cd8527a4e9f096ad93cfd158469cb01
yanick.salzmann <yanick.salzmann@gmail.com>
2019-10-13 17:47:00 +0200

    Update git-watcher script

--- scripts/git-watcher.sh
+++ scripts/git-watcher.sh
@@ -1,5 +1,7 @@
 #/bin/bash
 
+git remote update
+
 UPSTREAM=${1:-'@{u}'}
 LOCAL=$(git rev-parse @)
 REMOTE=$(git rev-parse "$UPSTREAM")
@@ -8,5 +10,9 @@ BASE=$(git merge-base @ "$UPSTREAM")
 if [ $LOCAL = $REMOTE ]; then
     echo "Up-to-date"
 elif [ $LOCAL = $BASE ]; then
-    echo "Need to pull"
+    echo "Need to pull local=$LOCAL remote=$REMOTE"
+    git pull origin master
+    mkdir -p cmake-build-debug
+    (cd cmake-build-debug && /usr/local/bin/cmake ..)
+    (cd cmake-build-debug && /usr/local/bin/cmake --build .)
 fi
