3e9eb1558b7433c71a7cc4177c0be99915aa8feb
yanick.salzmann <yanick.salzmann@gmail.com>
2019-10-14 09:47:05 +0200

    Trying to connect to bluetooth devices

--- camera_slave/src/main.cpp
+++ camera_slave/src/main.cpp
@@ -5,8 +5,9 @@ namespace carpi {
     int main(int argc, char* argv[]) {
         utils::log::Logger log{"main"};
         bluetooth::BluetoothManager mgr{};
-        for(const auto& device : mgr.scan_devices(std::chrono::seconds{10})) {
+        for(auto device : mgr.scan_devices(std::chrono::seconds{10})) {
             log->info("Device: {} @ {}", device.device_name(), device.address_string());
+            device.connect(0x80, 0x1234);
         }
 
         return 0;
