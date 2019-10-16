482f4ef8f2c3b9a27a0035c2a3a3c2b0f4bae6f3
yanick.salzmann <yanick.salzmann@gmail.com>
2019-10-16 13:15:46 +0200

    Implemented legacy frame parsing for OBD

--- obd/include/obd/msg/ObdFrame.hpp
+++ obd/include/obd/msg/ObdFrame.hpp
@@ -8,7 +8,10 @@
 namespace carpi::obd::msg {
     class ObdFrame {
         std::string _raw;
+        std::vector<uint8_t> _data;
+        uint32_t _priority = 0;
         uint32_t _tx_id = 0;
+        uint32_t _rx_id = 0;
 
     public:
         explicit ObdFrame(std::string raw) : _raw(std::move(raw)) { }
@@ -17,8 +20,40 @@ namespace carpi::obd::msg {
             return _tx_id;
         }
 
+        [[nodiscard]] uint32_t rx_id() const {
+            return _rx_id;
+        }
+
+        [[nodiscard]] uint32_t priority() const {
+            return _priority;
+        }
+
         [[nodiscard]] std::string raw() const {
             return _raw;
+        }
+
+        [[nodiscard]] std::vector<uint8_t> data() const {
+            return _data;
+        }
+
+        ObdFrame& tx_id(uint32_t tx_id) {
+            _tx_id = tx_id;
+            return *this;
+        }
+
+        ObdFrame& rx_id(uint32_t rx_id) {
+            _rx_id = rx_id;
+            return *this;
+        }
+
+        ObdFrame& priority(uint32_t priority) {
+            _priority = priority;
+            return *this;
+        }
+
+        ObdFrame& data(std::vector<uint8_t> data) {
+            _data = std::move(data);
+            return *this;
         }
 
         [[nodiscard]] std::string to_string() const {
--- obd/src/protocols/AbstractLegacyProtocol.cpp
+++ obd/src/protocols/AbstractLegacyProtocol.cpp
@@ -16,16 +16,24 @@ namespace carpi::obd::protocols {
             return false;
         }
 
-        utils::BinaryStream stream{utils::hex2bytes(raw)};
-        if(stream.size() < 6) {
-            log->debug("Received frame that is too small. Frame {} has size {}, needs to be at least 6", frame, stream.size());
+        const auto raw_data = utils::hex2bytes(raw);
+        if (raw_data.size() < 6) {
+            log->debug("Received frame that is too small. Frame {} has size {}, needs to be at least 6", frame, raw_data.size());
             return false;
         }
 
-        if(stream.size() > 11) {
-            log->debug("Received frame that is too long. Frame {} has size {}, needs to be less than 11", frame, stream.size());
+        if (raw_data.size() > 11) {
+            log->debug("Received frame that is too long. Frame {} has size {}, needs to be less than 11", frame, raw_data.size());
             return false;
         }
+
+        std::vector<uint8_t> frame_data{};
+        frame_data.assign(raw_data.begin() + 3, raw_data.end() - 1);
+
+        frame.data(frame_data)
+                .priority(raw_data[0])
+                .rx_id(raw_data[1])
+                .tx_id(raw_data[2]);
 
         return true;
     }
