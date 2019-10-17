b6814643de7dbc44b6cc4cd036e4057daf800359
yanick.salzmann <yanick.salzmann@gmail.com>
2019-10-17 13:24:08 +0200

    Changing carpi_master to test ELM327 adapter

--- carpi_master/src/main.cpp
+++ carpi_master/src/main.cpp
@@ -1,13 +1,24 @@
 #include <iostream>
 #include <video_stream/H264Conversion.hpp>
+#include <bluetooth_utils/BluetoothManager.hpp>
 #include "comm/CommServer.hpp"
 
 namespace carpi {
     int main(int argc, char* argv[]) {
         video::H264Conversion::initialize_ffmpeg();
         CommServer server{};
+        bluetooth::BluetoothManager btmgr{};
+
+        utils::Logger log{"main"};
+
         std::string line{};
         do {
+            const auto devices = btmgr.scan_devices(5);
+            uint32_t index = 1;
+            for(const auto& device : devices) {
+                log->info("{}) {}", index, device);
+            }
+
             std::getline(std::cin, line);
         } while(line != "q");
 
