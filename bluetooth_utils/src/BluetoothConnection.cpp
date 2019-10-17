7a6ad7d2a41aa60190e1ec45486f67c0efcd12e1
yanick.salzmann <yanick.salzmann@gmail.com>
2019-10-17 17:19:12 +0200

    properly handling non blocking sockets

--- bluetooth_utils/src/BluetoothConnection.cpp
+++ bluetooth_utils/src/BluetoothConnection.cpp
@@ -62,10 +62,10 @@ namespace carpi::bluetooth {
 
     std::size_t BluetoothConnection::read_some(void *buffer, std::size_t max_bytes) {
         const auto num_read = recv(_socket, buffer, max_bytes, 0);
-        if(num_read < 0 && num_read != EAGAIN) {
+        if(num_read < 0 && errno != EAGAIN) {
             log->error("Error reading any data: {} (errno={})", utils::error_to_string(), errno);
             throw std::runtime_error("Error reading any data");
-        } else if(num_read == EAGAIN) {
+        } else if(errno == EAGAIN) {
             return 0;
         }
 
