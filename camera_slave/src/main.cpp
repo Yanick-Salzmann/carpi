d31012041c317be3a97a75ae06f3f11e5ca675cb
yanick.salzmann <yanick.salzmann@gmail.com>
2019-10-17 07:55:56 +0200

    Testing if mmal version is working.

--- camera_slave/src/main.cpp
+++ camera_slave/src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <bluetooth_utils/BluetoothManager.hpp>
 #include <bluetooth_utils/BluetoothConnection.hpp>
+#include <interface/mmal/mmal.h>
 
 namespace carpi {
     int main(int argc, char* argv[]) {
@@ -11,6 +12,8 @@ namespace carpi {
         getlogin_r(username, LOGIN_NAME_MAX);
 
         utils::Logger log{"main"};
+        log->info("MMAL v{}.{}", MMAL_VERSION_MAJOR, MMAL_VERSION_MINOR);
+
         bluetooth::BluetoothManager mgr{};
         for(auto device : mgr.scan_devices(std::chrono::seconds{10})) {
             log->info("Device: {} @ {}", device.device_name(), device.address_string());
