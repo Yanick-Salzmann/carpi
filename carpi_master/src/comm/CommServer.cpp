79ad007519e9f0badd91452d08c125d2fc3e466a
yanick.salzmann <yanick.salzmann@gmail.com>
2019-10-17 15:09:53 +0200

    Typo in code

--- carpi_master/src/comm/CommServer.cpp
+++ carpi_master/src/comm/CommServer.cpp
@@ -35,7 +35,7 @@ namespace carpi {
                 .tv_usec = 0
             };
 
-            const auto rc = select(max_socket + 1, &socket_set, nullptr, nullptr, &timeout);ß
+            const auto rc = select(max_socket + 1, &socket_set, nullptr, nullptr, &timeout);
 
             if(rc < 0) {
                 log->error("Error selecting from sockets: {} (errno={})", utils::error_to_string(errno), errno);
