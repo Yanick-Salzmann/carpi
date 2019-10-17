f20ff622be14cfbc6c1114fcf7ffa6e547f1334d
yanick.salzmann <yanick.salzmann@gmail.com>
2019-10-17 20:23:53 +0200

    Debug output for voltage string

--- obd/src/ObdInterface.cpp
+++ obd/src/ObdInterface.cpp
@@ -175,6 +175,8 @@ namespace carpi::obd {
             if(voltage_string[0] == 'v' || voltage_string[0] == 'V') {
                 voltage_string = voltage_string.substr(1); // skip v prefix
             }
+
+            log->info("Trying to parse OBD voltage from: {}", voltage_string);
             const auto voltage = utils::lexical_cast<float>(voltage_string);
             return voltage >= 6.0f;
         } catch (std::bad_cast &) {
