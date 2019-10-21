038c410bb266cc91deca30c10d958cae75a72463
yanick.salzmann <yanick.salzmann@gmail.com>
2019-10-21 19:32:47 +0200

    More mmal camera development

--- camera_slave/src/camera/RawCameraStream.cpp
+++ camera_slave/src/camera/RawCameraStream.cpp
@@ -3,16 +3,121 @@
 
 #include "interface/mmal/util/mmal_default_components.h"
 
+#define MMAL_CAMERA_VIDEO_PORT 1
+#define MMAL_CAMERA_CAPTURE_PORT 2
+#define VIDEO_FRAME_RATE_DEN 1
+#define VIDEO_OUTPUT_BUFFERS_NUM 3
+
 namespace carpi::camera {
     LOGGER_IMPL(RawCameraStream);
 
-    void RawCameraStream::initialize_camera() {
+    void RawCameraStream::initialize_camera(const CameraConfiguration &configuration) {
         MMAL_COMPONENT_T *camera = nullptr;
 
-        const auto status = mmal_component_create(MMAL_COMPONENT_DEFAULT_CAMERA, &camera);
+        auto status = mmal_component_create(MMAL_COMPONENT_DEFAULT_CAMERA, &camera);
         if (status != MMAL_SUCCESS) {
             log->error("Error creating MMAL camera component: {} (error={})", utils::error_to_string(status), status);
             throw std::runtime_error("Error setting up camera");
+        }
+
+        std::shared_ptr<MMAL_COMPONENT_T> camera_component{camera, [](MMAL_COMPONENT_T *cam) { mmal_component_destroy(cam); }};
+
+        if (camera->output_num == 0) {
+            log->error("MMAL camera component does not have any output ports");
+            throw std::runtime_error("Error setting up camera");
+        }
+
+        const auto video_port = camera->output[MMAL_CAMERA_VIDEO_PORT];
+
+        MMAL_PARAMETER_CAMERA_CONFIG_T cam_config{};
+        cam_config.hdr.id = MMAL_PARAMETER_CAMERA_CONFIG;
+        cam_config.hdr.size = sizeof cam_config;
+        cam_config.max_stills_h = configuration.height();
+        cam_config.max_stills_w = configuration.width();
+        cam_config.stills_yuv422 = MMAL_FALSE;
+        cam_config.one_shot_stills = MMAL_FALSE;
+        cam_config.max_preview_video_w = configuration.width();
+        cam_config.max_preview_video_h = configuration.height();
+        cam_config.num_preview_video_frames = configuration.preview_fps();
+        cam_config.stills_capture_circular_buffer_height = 0;
+        cam_config.fast_preview_resume = MMAL_FALSE;
+        cam_config.use_stc_timestamp = MMAL_PARAM_TIMESTAMP_MODE_RESET_STC;
+
+        status = mmal_port_parameter_set(camera->control, &cam_config.hdr);
+        if (status != MMAL_SUCCESS) {
+            log->error("Error sending camera parameter config to camera: {} (errno={})", utils::error_to_string(status), status);
+            throw std::runtime_error("Error setting up camera");
+        }
+
+        MMAL_PARAMETER_CHANGE_EVENT_REQUEST_T change_event_request = {
+                {MMAL_PARAMETER_CHANGE_EVENT_REQUEST, sizeof(MMAL_PARAMETER_CHANGE_EVENT_REQUEST_T)},
+                MMAL_PARAMETER_CAMERA_SETTINGS,
+                1
+        };
+
+        status = mmal_port_parameter_set(camera->control, &change_event_request.hdr);
+        if (status != MMAL_SUCCESS) {
+            log->error("Error sending parameter change event to camera: {} (errno={})", utils::error_to_string(status), status);
+            throw std::runtime_error("Error setting up camera");
+        }
+
+        status = mmal_port_enable(camera->control, &RawCameraStream::camera_control_callback);
+        if(status != MMAL_SUCCESS) {
+            log->error("Error sending camera event callback to camera: {} (errno={})", utils::error_to_string(status), status);
+            throw std::runtime_error("Error setting up camera");
+        }
+
+        auto format = video_port->format;
+
+        format->es->video.width = VCOS_ALIGN_UP(configuration.width(), 32u);
+        format->es->video.height = VCOS_ALIGN_UP(configuration.height(), 16u);
+        format->es->video.crop.x = 0;
+        format->es->video.crop.y = 0;
+        format->es->video.crop.width = format->es->video.width;
+        format->es->video.crop.height = format->es->video.height;
+        format->es->video.frame_rate.num = configuration.fps();
+        format->es->video.frame_rate.den = VIDEO_FRAME_RATE_DEN;
+
+        status = mmal_port_format_commit(video_port);
+        if (status != MMAL_SUCCESS) {
+            log->error("Error setting video format: {} (error={})", utils::error_to_string(status), status);
+            throw std::runtime_error("Error setting up camera");
+        }
+
+        video_port->userdata = reinterpret_cast<struct MMAL_PORT_USERDATA_T *>(this);
+    }
+
+    uint32_t RawCameraStream::map_format(VideoFormat format) {
+        switch (format) {
+            default:
+                return MMAL_ENCODING_I420;
+        }
+    }
+
+    void RawCameraStream::camera_control_callback(MMAL_PORT_T *port, MMAL_BUFFER_HEADER_T *buffer) {
+        std::shared_ptr<MMAL_BUFFER_HEADER_T> p{buffer, [](auto *buff) { mmal_buffer_header_release(buff); }};
+
+        if(port->userdata == nullptr) {
+            return;
+        }
+
+        auto* camera_stream = (RawCameraStream*) port->userdata;
+        camera_stream->handle_camera_control(port, buffer);
+    }
+
+    void RawCameraStream::handle_camera_control(MMAL_PORT_T *port, MMAL_BUFFER_HEADER_T *buffer) {
+        switch(buffer->cmd) {
+            case MMAL_EVENT_ERROR: {
+                const auto status = *(MMAL_STATUS_T*) buffer->data;
+                log->error("Error event received from camera. No data will be received. Error = {}, errno = {}", utils::error_to_string(status), status);
+                break;
+            }
+
+            case MMAL_EVENT_EOS: {
+                const auto eos_event = (MMAL_EVENT_END_OF_STREAM_T*) buffer->data;
+                log->info("Camera stream on port {} of type {} ended", eos_event->port_index, eos_event->port_type);
+                break;
+            }
         }
     }
 }
--- camera_slave/src/camera/RawCameraStream.hpp
+++ camera_slave/src/camera/RawCameraStream.hpp
@@ -7,11 +7,68 @@
 #include <common_utils/log.hpp>
 
 namespace carpi::camera {
+    enum class VideoFormat {
+        H264
+    };
+
+    class CameraConfiguration {
+        uint32_t _width;
+        uint32_t _height;
+
+        uint32_t _preview_fps;
+        uint32_t _fps;
+
+    public:
+        CameraConfiguration(uint32_t width, uint32_t height, uint32_t preview_fps, uint32_t fps) : _width(width), _height(height), _preview_fps(preview_fps), _fps(fps) {}
+
+        [[nodiscard]] uint32_t width() const {
+            return _width;
+        }
+
+        CameraConfiguration& width(uint32_t width) {
+            _width = width;
+            return *this;
+        }
+
+        [[nodiscard]] uint32_t height() const {
+            return _height;
+        }
+
+        CameraConfiguration& height(uint32_t height) {
+            _height = height;
+            return *this;
+        }
+
+        [[nodiscard]] uint32_t preview_fps() const {
+            return _preview_fps;
+        }
+
+        CameraConfiguration& preview_fps(uint32_t preview_fps) {
+            _preview_fps = preview_fps;
+            return *this;
+        }
+
+        [[nodiscard]] uint32_t fps() const {
+            return _fps;
+        }
+
+        CameraConfiguration& fps(uint32_t fps) {
+            _fps = fps;
+            return *this;
+        }
+    };
+
     class RawCameraStream {
         LOGGER;
 
+        static uint32_t map_format(VideoFormat format);
+
+        static void camera_control_callback(MMAL_PORT_T* port, MMAL_BUFFER_HEADER_T* buffer);
+
+        void handle_camera_control(MMAL_PORT_T *port, MMAL_BUFFER_HEADER_T *buffer);
+
     private:
-        void initialize_camera();
+        void initialize_camera(const CameraConfiguration& configuration);
     };
 };
 
