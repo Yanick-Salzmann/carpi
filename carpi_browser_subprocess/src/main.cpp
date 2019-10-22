3ee76c8e9e4c4ecd76b6d813cc2c18ba0f28e17b
yanick.salzmann <yanick.salzmann@gmail.com>
2019-10-22 19:46:52 +0200

    Started implementing cef browser subprocess

--- carpi_browser_subprocess/src/main.cpp
+++ carpi_browser_subprocess/src/main.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
 
 #include <include/cef_app.h>
+#include <common_utils/log.hpp>
+
+class MainApp : public CefApp {
+    IMPLEMENT_REFCOUNTING(MainApp);
+};
 
 int main(int argc, char* argv[]) {
+    CefMainArgs main_args{argc, argv};
+    CefRefPtr<MainApp> app{new MainApp{}};
 
+    return CefExecuteProcess(main_args, app.get(), nullptr);
 }
