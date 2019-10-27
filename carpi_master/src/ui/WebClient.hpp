e26b97a333d6049b2e5c790a532b86109cb68e42
yanick.salzmann <yanick.salzmann@gmail.com>
2019-10-27 17:06:27 +0100

    CEF local resource loading

--- carpi_master/src/ui/WebClient.hpp
+++ carpi_master/src/ui/WebClient.hpp
@@ -5,11 +5,17 @@
 #include <X11/Xlib.h>
 
 namespace carpi::ui {
-class WebClient : public CefClient {
+class WebClient : public CefClient, public CefLifeSpanHandler {
         IMPLEMENT_REFCOUNTING(WebClient);
     public:
+    CefRefPtr<CefLifeSpanHandler> GetLifeSpanHandler() override {
+        return this;
+    }
 
-    };
+    void OnAfterCreated(CefRefPtr<CefBrowser> browser) override {
+        browser->GetMainFrame()->LoadURL(CefString("local://ui/test.html"));
+    }
+};
 }
 
 #endif //CARPI_CARPI_WEBCLIENT_HPP
