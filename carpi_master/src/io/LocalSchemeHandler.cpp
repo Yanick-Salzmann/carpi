acde191026da39463f58ebbe51d1cf6286ccdbc0
yanick.salzmann <yanick.salzmann@gmail.com>
2019-10-27 16:50:01 +0100

    CEF local resource loading

--- carpi_master/src/io/LocalSchemeHandler.cpp
+++ carpi_master/src/io/LocalSchemeHandler.cpp
@@ -110,15 +110,15 @@ namespace carpi::io {
     }
 
     bool LocalSchemeHandler::Read(void *data_out, int bytes_to_read, int &bytes_read, CefRefPtr<CefResourceReadCallback> callback) {
-        log->info("Position: {}, File Size: {}, To Read: {}", _position, _file_size, bytes_to_read);
         const auto available = (_position < _file_size) ? (_file_size - _position) : 0;
+        log->info("Position: {}, File Size: {}, To Read: {}, Available: {}", _position, _file_size, bytes_to_read, available);
         if (available <= 0) {
             bytes_read = 0;
             return false;
         }
 
         const auto to_read = std::min<int64_t>(bytes_to_read, available);
-        log->info("Reading: {}", available);
+        log->info("Reading: {}", to_read);
         fread(data_out, 1, to_read, _fd);
         _position += to_read;
         bytes_read = to_read;
