2ff1757fbe2dbe052c5e65fd52b27c02ddb99054
yanick.salzmann <yanick.salzmann@gmail.com>
2019-10-27 06:08:20 +0100

    Added some more IPC logic, changed log level to verbose

--- carpi_master/src/comm/IpcClientManager.cpp
+++ carpi_master/src/comm/IpcClientManager.cpp
@@ -1,28 +1,64 @@
 #include "IpcClientManager.hpp"
-#include "../../../ipc_common/include/ipc_common/IpcPackage.hpp"
-#include "../../../ipc_common/include/ipc_common/IpcType.hpp"
+#include "ipc_common/IpcPackage.hpp"
+#include "ipc_common/IpcType.hpp"
+#include "IpcConnection.hpp"
 
 namespace carpi::comm {
     LOGGER_IMPL(IpcClientManager);
 
     void IpcClientManager::accept_connection(int connection, sockaddr_un addr) {
-        fd_set socket_set{};
-        FD_SET(connection, &socket_set);
-        struct timeval read_timeout{
-            .tv_sec = 0,
-            .tv_usec = 100000
-        };
-
-        const auto ret = select(connection, &socket_set, nullptr, nullptr, &read_timeout);
-        if(ret <= 0) {
-            log->warn("Discarding connection from {} because there was no response in 100 milliseconds", addr.sun_path);
+        ipc::IpcType type;
+        if(!read_type_from_socket(connection, type)) {
+            // read_type_from_socket is supposed to print the information
             return;
         }
 
-        auto init_package = ipc::IpcPackage::read_from_socket(connection);
+        _connections.emplace(type, std::make_shared<IpcConnection>());
+    }
+
+    void IpcClientManager::accept_bluetooth(std::shared_ptr<bluetooth::BluetoothConnection> connection) {
+        ipc::IpcType type;
+        if(!read_type_from_socket(connection->fd(), type)) {
+            // read_type_from_socket is supposed to print enough information
+            return;
+        }
+
+        _connections.emplace(type, std::make_shared<IpcConnection>());
+    }
+
+    bool IpcClientManager::read_type_from_socket(int socket, ipc::IpcType &out_type) {
+        fd_set socket_set{};
+        FD_SET(socket, &socket_set);
+        struct timeval read_timeout{
+                .tv_sec = 0,
+                .tv_usec = 100000
+        };
+
+        const auto ret = select(socket + 1, &socket_set, nullptr, nullptr, &read_timeout);
+        if(ret <= 0) {
+            sockaddr addr{};
+            socklen_t sock_len;
+            getsockname(socket, &addr, &sock_len);
+            std::string name{};
+            if(addr.sa_family == AF_BLUETOOTH) {
+                char bt_addr[18]{};
+                ba2str(&((sockaddr_rc*) &addr)->rc_bdaddr, bt_addr);
+                bt_addr[17] = '\0';
+                name = bt_addr;
+            } else if(addr.sa_family == AF_UNIX) {
+                name = ((sockaddr_un*) &addr)->sun_path;
+            } else {
+                name = std::to_string(socket);
+            }
+
+            log->warn("Discarding connection from {} because there was no response in 100 milliseconds", name);
+            return false;
+        }
+
+        auto init_package = ipc::IpcPackage::read_from_socket(socket);
         if(init_package.opcode() != ipc::Opcodes::MSG_IPC_TYPE) {
             log->warn("First package from IPC client must be of type MSG_IPC_TYPE(=0) but was {}", static_cast<uint32_t>(init_package.opcode()));
-            return;
+            return false;
         }
 
         ipc::IpcType type;
@@ -30,7 +66,10 @@ namespace carpi::comm {
 
         if(type >= ipc::IpcType::FIRST_UNKNOWN_IPC_TYPE) {
             log->warn("Invalid IPC init packet received");
-            return;
+            return false;
         }
+
+        out_type = type;
+        return true;
     }
 }
--- carpi_master/src/comm/IpcClientManager.hpp
+++ carpi_master/src/comm/IpcClientManager.hpp
@@ -5,17 +5,26 @@
 #include <common_utils/log.hpp>
 
 #include <vector>
+#include <map>
 #include <sys/socket.h>
 #include <sys/un.h>
+#include <ipc_common/IpcType.hpp>
+#include <bluetooth_utils/BluetoothConnection.hpp>
 
 namespace carpi::comm {
+    class IpcConnection;
+    typedef std::shared_ptr<IpcConnection> IpcConnectionPtr;
+
     class IpcClientManager : public utils::Singleton<IpcClientManager> {
         LOGGER;
 
-        std::vector<int> _pending_connections;
+        std::map<ipc::IpcType, IpcConnectionPtr> _connections;
+
+        bool read_type_from_socket(int socket, ipc::IpcType& out_type);
 
     public:
         void accept_connection(int connection, sockaddr_un addr);
+        void accept_bluetooth(std::shared_ptr<bluetooth::BluetoothConnection> connection);
     };
 }
 
