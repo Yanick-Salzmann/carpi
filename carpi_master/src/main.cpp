2c4dcd161e10381e340725f1639a6cd1359846df
yanick.salzmann <yanick.salzmann@gmail.com>
2019-10-27 06:44:17 +0100

    Trying to convert video files

--- carpi_master/src/main.cpp
+++ carpi_master/src/main.cpp
@@ -8,8 +8,35 @@
 #include <obd/ObdInterface.hpp>
 #include <obd/ObdCommandList.hpp>
 #include <ui/WebCore.hpp>
+#include <video_stream/H264Stream.hpp>
 
 #include "main.hpp"
+#include <fstream>
+
+class MemoryStreamSource : public carpi::video::IStreamSource {
+    LOGGER;
+
+    std::vector<uint8_t> _data;
+    std::size_t _position = 0;
+
+public:
+    explicit MemoryStreamSource(std::vector<uint8_t> data) : _data(std::move(data)) { }
+
+    size_t read(void *buffer, std::size_t num_bytes) override {
+        log->info("Reading {} bytes from stream", num_bytes);
+
+        if(_position >= _data.size()) {
+            return 0;
+        }
+
+        const auto to_read = std::min(num_bytes, _data.size() - _position);
+        memcpy(buffer, &_data[_position], to_read);
+        _position += to_read;
+        return to_read;
+    }
+};
+
+LOGGER_IMPL(MemoryStreamSource);
 
 namespace carpi {
     int _argc;
@@ -19,9 +46,23 @@ namespace carpi {
         _argc = argc;
         _argv = argv;
 
-        ui::WebCore core{};
+        //ui::WebCore core{};
 
         video::H264Conversion::initialize_ffmpeg();
+
+        std::ifstream is{"camera.h264", std::ios::binary};
+        is.seekg(0, std::ios::end);
+        const auto data_size = is.tellg();
+        is.seekg(0, std::ios::beg);
+        std::vector<uint8_t> data(data_size);
+        is.read((char*) data.data(), data_size);
+        const auto stream_source = std::make_shared<MemoryStreamSource>(data);
+        const auto stream = std::make_shared<video::H264Stream>(stream_source, 1920, 1080, 30);
+
+        std::ofstream os{"camera.ogg", std::ios::binary};
+
+        video::H264Conversion conversion{stream, ".ogg", [&os](void* data, std::size_t size) { os.write((const char*) data, size); }, [&os]() { os.close(); }};
+
         CommServer server{};
 
         utils::Logger log{"main"};
@@ -30,7 +71,7 @@ namespace carpi {
         std::string line{};
         std::getline(std::cin, line);
 
-        core.manual_shutdown();
+        //core.manual_shutdown();
 
         server.shutdown_acceptor();
         return 0;
