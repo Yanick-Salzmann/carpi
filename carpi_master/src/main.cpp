06b6447c351cdbb42a3acc1950ebed4f7debf57b
yanick.salzmann <yanick.salzmann@gmail.com>
2019-10-31 20:26:56 +0100

    DHT sensor stuff

--- carpi_master/src/main.cpp
+++ carpi_master/src/main.cpp
@@ -13,6 +13,8 @@
 #include "main.hpp"
 #include <fstream>
 
+#include <wiringPi.h>
+
 class MemoryStreamSource : public carpi::video::IStreamSource {
     LOGGER;
 
@@ -40,7 +42,70 @@ namespace carpi {
     int _argc;
     char **_argv;
 
+    void temperature_loop() {
+#define MAXTIMINGS	85
+#define DHTPIN		23
+        int dht11_dat[5] = { 0, 0, 0, 0, 0 };
+
+        uint8_t laststate	= HIGH;
+        uint8_t counter		= 0;
+        uint8_t j		= 0, i;
+        float	f;
+
+        dht11_dat[0] = dht11_dat[1] = dht11_dat[2] = dht11_dat[3] = dht11_dat[4] = 0;
+
+        pinMode( DHTPIN, OUTPUT );
+        digitalWrite( DHTPIN, LOW );
+        delay( 18 );
+        digitalWrite( DHTPIN, HIGH );
+        delayMicroseconds( 40 );
+        pinMode( DHTPIN, INPUT );
+
+        for ( i = 0; i < MAXTIMINGS; i++ )
+        {
+            counter = 0;
+            while ( digitalRead( DHTPIN ) == laststate )
+            {
+                counter++;
+                delayMicroseconds( 1 );
+                if ( counter == 255 )
+                {
+                    break;
+                }
+            }
+            laststate = digitalRead( DHTPIN );
+
+            if ( counter == 255 )
+                break;
+
+            if ( (i >= 4) && (i % 2 == 0) )
+            {
+                dht11_dat[j / 8] <<= 1;
+                if ( counter > 16 )
+                    dht11_dat[j / 8] |= 1;
+                j++;
+            }
+        }
+
+        if ( (j >= 40) &&
+             (dht11_dat[4] == ( (dht11_dat[0] + dht11_dat[1] + dht11_dat[2] + dht11_dat[3]) & 0xFF) ) )
+        {
+            printf( "\rHumidity = %02d.%02d %% Temperature = %02d.%02d C",
+                    dht11_dat[0], dht11_dat[1], dht11_dat[2], dht11_dat[3]);
+        }else  {
+            printf( "\rData not good, skip                                       " );
+        }
+    }
+
     int main(int argc, char *argv[]) {
+        if ( wiringPiSetup() == -1 )
+            exit( 1 );
+
+        while(true) {
+            temperature_loop();
+            delay(1000);
+        }
+
         _argc = argc;
         _argv = argv;
 
