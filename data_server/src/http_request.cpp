432c3e5ba02e318f70ff8427ec42fb192ced1d23
yanick.salzmann <yanick.salzmann@gmail.com>
2019-11-01 16:20:03 +0100

    Data server stuff

--- data_server/src/http_request.cpp
+++ data_server/src/http_request.cpp
@@ -38,8 +38,6 @@ namespace carpi::data {
             return;
         }
 
-        log->info("Returning file: {}", ui_file.string());
         HttpResponse{HttpStatusCode::OK, "OK"}.respond_with_file(ui_file.string()).write_to_socket(socket);
-        log->info("Responded to HTTP request: {}", path);
     }
 }
