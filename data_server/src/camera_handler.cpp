aef958139a32b15bf58e81f71f742ce71d508991
yanick.salzmann <yanick.salzmann@gmail.com>
2019-11-03 21:03:07 +0100

    Debugging camera handler

--- data_server/src/camera_handler.cpp
+++ data_server/src/camera_handler.cpp
@@ -5,8 +5,21 @@ namespace carpi::data {
     LOGGER_IMPL(CameraHandler);
 
     void CameraHandler::begin_streaming(const std::function<bool(void *, std::size_t)> &data_callback) {
-        _data_listeners.emplace_back(data_callback);
         initialize_camera();
+
+        std::shared_ptr <ReaderContext> context = std::make_shared<ReaderContext>();
+        {
+            std::lock_guard <std::mutex> l{_listener_lock};
+            _data_listeners.emplace(context);
+        }
+
+        context->last_read_index = _write_index;
+        context->converter = std::make_shared<video::H264Conversion>(
+                std::make_shared<video::H264Stream>(std::make_shared<StreamSource>(*this, context), 1920, 1080, 30),
+                "mp4",
+                [this, context](void *data, std::size_t size) { handle_conversion_data(context, data, size); },
+                []() {}
+        );
     }
 
     void CameraHandler::initialize_camera() {
@@ -15,30 +28,20 @@ namespace carpi::data {
             return;
         }
 
-        auto future = std::async(std::launch::async, [this]() {
-            _converter = std::make_shared<video::H264Conversion>(
-                    std::make_shared<video::H264Stream>(std::make_shared<StreamSource>(*this), 1920, 1080, 30),
-                    "mp4",
-                    [this](void *data, std::size_t size) { handle_conversion_data(data, size); },
-                    []() {}
-            );
-        });
-
 
         _camera_stream = std::make_shared<video::RawCameraStream>(
                 [this](const std::vector<uint8_t> &data, std::size_t size) { handle_camera_frame(data, size); }
         );
 
         _camera_stream->initialize_camera({1920, 1080, 1, 30});
-        future.get();
     }
 
-    size_t CameraHandler::read(void *buffer, std::size_t num_bytes) {
-        if (!_partial_frame.empty()) {
-            if (_partial_position < _partial_frame.size()) {
-                const auto to_read = std::min<std::size_t>(num_bytes, _partial_frame.size() - _partial_position);
-                memcpy(buffer, &_partial_frame[_partial_position], to_read);
-                _partial_position += to_read;
+    size_t CameraHandler::read(ReaderContext* context, void *buffer, std::size_t num_bytes) {
+        if (!context->partial_frame.empty()) {
+            if (context->partial_position < context->partial_frame.size()) {
+                const auto to_read = std::min<std::size_t>(num_bytes, context->partial_frame.size() - context->partial_position);
+                memcpy(buffer, &context->partial_frame[context->partial_position], to_read);
+                context->partial_position += to_read;
                 return to_read;
             }
         }
@@ -46,17 +49,17 @@ namespace carpi::data {
         std::vector<uint8_t> frame_data{};
         {
             std::unique_lock<std::mutex> l{_frame_lock};
-            if (_frame_queue.empty()) {
-                _frame_event.wait(l, [this]() { return !_frame_queue.empty(); });
+            if (context->last_read_index == _write_index) {
+                _frame_event.wait(l, [this, context]() { return context->last_read_index != _write_index; });
             }
 
-            frame_data = _frame_queue.front();
-            _frame_queue.pop_front();
+            frame_data = _frame_queue[context->last_read_index++];
+            context->last_read_index %= QUEUE_SIZE;
         }
 
         if (frame_data.size() > num_bytes) {
-            _partial_frame = frame_data;
-            _partial_position = num_bytes;
+            context->partial_frame = frame_data;
+            context->partial_position = num_bytes;
         }
 
         const auto to_read = std::min<std::size_t>(num_bytes, frame_data.size());
@@ -67,23 +70,20 @@ namespace carpi::data {
 
     void CameraHandler::handle_camera_frame(const std::vector<uint8_t> &data, std::size_t size) {
         std::lock_guard<std::mutex> l{_frame_lock};
-        _frame_queue.emplace_back(data.begin(), data.begin() + size);
-        while (_frame_queue.size() > 60) {
-            _frame_queue.pop_front();
-        }
+        auto next_index = ++_write_index;
+        next_index %= QUEUE_SIZE;
+        _write_index = next_index;
 
+        _frame_queue[next_index].assign(data.begin(), data.begin() + size);
         _frame_event.notify_all();
     }
 
-    void CameraHandler::handle_conversion_data(void *data, std::size_t size) {
-        std::lock_guard<std::mutex> l{_listener_lock};
-        std::deque<std::function<bool(void *, std::size_t)>> listeners{};
-        for (const auto &listener : _data_listeners) {
-            if (listener(data, size)) {
-                listeners.push_back(listener);
-            }
+    void CameraHandler::handle_conversion_data(const std::shared_ptr<ReaderContext> &context, void *data, std::size_t size) {
+        if (context->callback(data, size)) {
+            return;
         }
 
-        _data_listeners = listeners;
+        std::lock_guard<std::mutex> l{_listener_lock};
+        _data_listeners.erase(context);
     }
 }
--- data_server/src/camera_handler.hpp
+++ data_server/src/camera_handler.hpp
@@ -6,25 +6,40 @@
 #include <video_stream/RawCameraStream.hpp>
 #include <deque>
 #include <memory>
+#include <array>
+#include <atomic>
+#include <set>
 
 #include <video_stream/H264Conversion.hpp>
 #include <video_stream/H264Stream.hpp>
 
 namespace carpi::data {
-    class CameraHandler : public utils::Singleton<CameraHandler>, public video::IStreamSource {
+    class CameraHandler : public utils::Singleton<CameraHandler> {
         LOGGER;
+
+        struct ReaderContext {
+            std::function<bool(void *, std::size_t)> callback;
+            std::shared_ptr<video::H264Conversion> converter;
+
+            std::size_t last_read_index{};
+            std::vector<uint8_t> partial_frame{};
+            std::size_t partial_position = 0;
+        };
 
         struct StreamSource : public video::IStreamSource {
             CameraHandler& parent;
+            ReaderContext* context;
 
-            explicit StreamSource(CameraHandler& parent) : parent(parent) { }
+            explicit StreamSource(CameraHandler& parent, ReaderContext* context) : parent(parent), context(context) { }
 
             size_t read(void *buffer, std::size_t num_bytes) override {
-                return parent.read(buffer, num_bytes);
+                return parent.read(context, buffer, num_bytes);
             }
         };
 
-        std::deque<std::function<bool(void *, std::size_t)>> _data_listeners;
+        static constexpr std::size_t QUEUE_SIZE = 300;
+
+        std::set<std::shared_ptr<ReaderContext>> _data_listeners;
         std::mutex _listener_lock;
 
         std::mutex _load_lock;
@@ -32,19 +47,19 @@ namespace carpi::data {
         std::shared_ptr<video::H264Conversion> _converter;
 
         std::mutex _frame_lock;
+
         std::condition_variable _frame_event;
-        std::deque<std::vector<uint8_t>> _frame_queue;
-        std::vector<uint8_t> _partial_frame;
-        std::size_t _partial_position = 0;
+        std::array<std::vector<uint8_t>, QUEUE_SIZE> _frame_queue;
+        std::atomic_size_t _write_index{0};
 
         void initialize_camera();
 
         void handle_camera_frame(const std::vector<uint8_t> &data, std::size_t size);
 
-        void handle_conversion_data(void *data, std::size_t size);
+        void handle_conversion_data(const std::shared_ptr<ReaderContext>& context, void *data, std::size_t size);
+        size_t read(ReaderContext* context, void *buffer, std::size_t num_bytes);
 
     public:
-        size_t read(void *buffer, std::size_t num_bytes) override;
 
         void begin_streaming(const std::function<bool(void *, std::size_t)> &data_callback);
     };
