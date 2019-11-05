5db2759a8c458209c5ffebeed52abb3d6dfea541
yanick.salzmann <yanick.salzmann@gmail.com>
2019-11-05 19:56:34 +0100

    Debugging camera handler

--- data_server/src/camera_handler.cpp
+++ data_server/src/camera_handler.cpp
@@ -41,6 +41,7 @@ namespace carpi::data {
     size_t CameraHandler::read(ReaderContext *context, void *buffer, std::size_t num_bytes) {
         if (!context->partial_frame.empty()) {
             if (context->partial_position < context->partial_frame.size()) {
+                log->info("Processing from partial frame");
                 const auto to_read = std::min<std::size_t>(num_bytes, context->partial_frame.size() - context->partial_position);
                 memcpy(buffer, &context->partial_frame[context->partial_position], to_read);
                 context->partial_position += to_read;
