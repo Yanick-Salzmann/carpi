3aad126970cfb2b3b705708583cff44752e6f9d5
yanick.salzmann <yanick.salzmann@gmail.com>
2019-11-06 20:21:32 +0100

    Using ffmpeg subprocess for video

--- data_server/src/camera_handler.cpp
+++ data_server/src/camera_handler.cpp
@@ -56,6 +56,7 @@ namespace carpi::data {
     void CameraHandler::handle_camera_frame(const std::vector<uint8_t> &data, std::size_t size) {
         std::lock_guard<std::mutex> l{_listener_lock};
         for (const auto &listener : _data_listeners) {
+            log->info("Writing {} bytes to output", size);
             write(listener->ffmpeg_process.stdin_pipe, data.data(), size);
         }
     }
