1032d778a819864750b0d9178595b31aec6d5b07
yanick.salzmann <yanick.salzmann@gmail.com>
2019-11-06 20:20:38 +0100

    Using ffmpeg subprocess for video

--- data_server/src/camera_handler.cpp
+++ data_server/src/camera_handler.cpp
@@ -15,7 +15,7 @@ namespace carpi::data {
         context->callback = data_callback;
         context->ffmpeg_process = utils::launch_subprocess(
                 "ffmpeg",
-                { "-f", "rawvideo", "-pix_fmt", "yuv420p", "-video_size", "1920x1088", "-r", "30", "-i", "-", "-c", "libx264", "-f", "mp4", "-movflags", "frag_keyframe+empty_moov", "-", "-loglevel", "debug"}
+                { "-f", "rawvideo", "-pix_fmt", "yuv420p", "-video_size", "1920x1088", "-r", "30", "-i", "-", "-c", "libx264", "-f", "mp4", "-movflags", "frag_keyframe+empty_moov", "-", "-loglevel", "trace"}
         );
         log->info("Launched ffmpeg process. PID: {}, error: {}", context->ffmpeg_process.process_id, context->ffmpeg_process.error_code);
 
