701a8e6490639a001e970eec3ab366a461bb4e6c
yanick.salzmann <yanick.salzmann@gmail.com>
2019-11-07 19:35:19 +0100

    Adding support for video ranges

--- carpi_master/src/ui/WebCore.cpp
+++ carpi_master/src/ui/WebCore.cpp
@@ -50,7 +50,7 @@ namespace carpi::ui {
         CefString(&settings.cache_path) = cache_path.string();
         settings.ignore_certificate_errors = 1;
         settings.windowless_rendering_enabled = 0;
-        settings.log_severity = LOGSEVERITY_DEFAULT;
+        settings.log_severity = LOGSEVERITY_ERROR;
         settings.no_sandbox = 1;
         settings.remote_debugging_port = 52100;
         CefString(&settings.locales_dir_path) = "/usr/local/bin/locales";
