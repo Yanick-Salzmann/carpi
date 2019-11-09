2bfe85810853a04551f228c2a9efd1048d5ba077
yanick.salzmann <yanick.salzmann@gmail.com>
2019-11-09 15:38:12 +0100

    WebGL approach

--- carpi_master/src/io/camera/camera_stream.hpp
+++ carpi_master/src/io/camera/camera_stream.hpp
@@ -3,7 +3,11 @@
 
 #include <common_utils/singleton.hpp>
 #include <common_utils/log.hpp>
+
+extern "C" {
 #include <libswscale/swscale.h>
+};
+
 #include <video_stream/RawCameraStream.hpp>
 #include <vector>
 #include <memory>
@@ -21,23 +25,26 @@ namespace carpi::io::camera {
 
         std::shared_ptr<video::RawCameraStream> _stream;
 
-        SwsContext* _sws_context;
+        SwsContext *_sws_context;
 
-        void handle_camera_frame(const std::vector<uint8_t>& data, std::size_t size);
+        void handle_camera_frame(const std::vector<uint8_t> &data, std::size_t size);
 
         void start_camera_streaming();
 
     public:
         CameraStream();
-        CameraStream(const CameraStream&) = delete;
-        CameraStream(CameraStream&&) = delete;
+
+        CameraStream(const CameraStream &) = delete;
+
+        CameraStream(CameraStream &&) = delete;
 
         ~CameraStream();
 
-        void operator = (const CameraStream&) = delete;
-        void operator = (CameraStream&&) = delete;
+        void operator=(const CameraStream &) = delete;
 
-        void camera_parameters(uint32_t& width, uint32_t& height, uint32_t& fps) {
+        void operator=(CameraStream &&) = delete;
+
+        void camera_parameters(uint32_t &width, uint32_t &height, uint32_t &fps) {
             width = CAMERA_WIDTH;
             height = CAMERA_HEIGHT;
             fps = CAMERA_FPS;
