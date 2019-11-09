745976e246a63a3f75124a53bd1b8e0f0e9a872e
yanick.salzmann <yanick.salzmann@gmail.com>
2019-11-09 17:35:59 +0100

    WebGL approach

--- carpi_master/src/ui/WebClient.cpp
+++ carpi_master/src/ui/WebClient.cpp
@@ -4,6 +4,7 @@ namespace carpi::ui {
     LOGGER_IMPL(WebClient);
 
     bool WebClient::OnQuery(CefRefPtr<CefBrowser> browser, CefRefPtr<CefFrame> frame, int64 query_id, const CefString &request, bool persistent, CefRefPtr<CefMessageRouterBrowserSide::Callback> callback) {
+        log->info("Received query named {} (persistent={})", request.ToString(), persistent);
         return false;
     }
 }
