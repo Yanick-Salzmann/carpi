d854992df01ae7b03e72adefb93ea8d70a3da531
yanick.salzmann <yanick.salzmann@gmail.com>
2019-11-09 19:57:31 +0100

    WebGL approach

--- carpi_master/ui/js/camera_frame.js
+++ carpi_master/ui/js/camera_frame.js
@@ -1,6 +1,4 @@
 $(() => {
-    console.log(window.fetch_raw_frame);
-
     const canvas = document.querySelector('#video-canvas');
     const gl = canvas.getContext('webgl');
     let texture = null;
@@ -128,12 +126,11 @@ $(() => {
     }
 
     function fetch_frame() {
-        setTimeout(() => {
+        console.log("fetch_frame");
+        setInterval(() => {
             const frame_data = fetch_raw_frame();
-            console.log(frame_data);
             gl.bindTexture(gl.TEXTURE_2D, texture);
             gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, 480, 368, 0, gl.RGBA, gl.UNSIGNED_BYTE, new Uint8Array(frame_data));
-            fetch_frame();
         }, 0);
     }
 
