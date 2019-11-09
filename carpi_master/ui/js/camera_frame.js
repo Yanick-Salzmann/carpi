b301729ec6839439553ae976be7b998c62ec6b1f
yanick.salzmann <yanick.salzmann@gmail.com>
2019-11-09 19:58:59 +0100

    WebGL approach

--- carpi_master/ui/js/camera_frame.js
+++ carpi_master/ui/js/camera_frame.js
@@ -129,6 +129,7 @@ $(() => {
         setInterval(() => {
             console.log("fetch_frame");
             const frame_data = fetch_raw_frame();
+            console.log(frame_data.byteLength);
             gl.bindTexture(gl.TEXTURE_2D, texture);
             gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, 480, 368, 0, gl.RGBA, gl.UNSIGNED_BYTE, new Uint8Array(frame_data));
         }, 0);
