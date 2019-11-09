44129550a0fd0545bfdf5abf4f5f98f263a48156
yanick.salzmann <yanick.salzmann@gmail.com>
2019-11-09 06:40:28 +0100

    Debugging video stuff

--- data_server/src/camera_handler.cpp
+++ data_server/src/camera_handler.cpp
@@ -13,7 +13,7 @@ namespace carpi::data {
         context->callback = data_callback;
         context->ffmpeg_process = utils::launch_subprocess(
                 "ffmpeg",
-                { "ffmpeg", "-f", "rawvideo", "-pix_fmt", "yuv420p", "-s", "1920x1080", "-r", "30", "-i", "-", "-c", "libx264", "-f", "mp4", "-movflags", "frag_keyframe+empty_moov", "-", "-loglevel", "error"}
+                { "ffmpeg", "-f", "rawvideo", "-pix_fmt", "yuv420p", "-s", "1920x1080", "-r", "30", "-i", "-", "-c", "copy", "-f", "mp4", "-movflags", "frag_keyframe+empty_moov", "-", "-loglevel", "error"}
         );
         log->info("Launched ffmpeg process. PID: {}, error: {}", context->ffmpeg_process.process_id, context->ffmpeg_process.error_code);
 
