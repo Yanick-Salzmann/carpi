1f867dfc7b566820082b897dd3d231190417e4e9
yanick.salzmann <yanick.salzmann@gmail.com>
2019-11-09 18:07:50 +0100

    WebGL approach

--- carpi_master/src/ui/WebClient.cpp
+++ carpi_master/src/ui/WebClient.cpp
@@ -6,10 +6,18 @@ namespace carpi::ui {
     LOGGER_IMPL(WebClient);
 
     bool WebClient::OnQuery(CefRefPtr<CefBrowser> browser, CefRefPtr<CefFrame> frame, int64 query_id, const CefString &request, bool persistent, CefRefPtr<CefMessageRouterBrowserSide::Callback> callback) {
+        if(request == "EventHandlerInit") {
+            // TODO: Register handler
+            return true;
+        }
+
         using nlohmann::json;
 
+        json req_obj = json::parse(request.ToString());
+        const std::string type = req_obj["type"];
+
         log->info("Received query named {} (persistent={})", request.ToString(), persistent);
-        if(request == "camera_parameters") {
+        if(type == "camera_parameters") {
             uint32_t width, height, fps;
             sCameraStream->camera_parameters(width, height, fps);
             json val{
