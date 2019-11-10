0fcfec85c7785ad2805e907c4bb0e1eb96cf6d1b
yanick.salzmann <yanick.salzmann@gmail.com>
2019-11-10 14:38:29 +0100

    More OBD codecs (0x0F - 0x1E)

--- obd/src/ObdCommandList.cpp
+++ obd/src/ObdCommandList.cpp
@@ -11,6 +11,9 @@
 #include <obd/response/SpeedCodec.hpp>
 #include <obd/response/TimingAdvanceCodec.hpp>
 #include <obd/response/EngineRuntimeCodec.hpp>
+#include <obd/response/AirflowRateCodec.hpp>
+#include <obd/response/ThrottlePositionCodec.hpp>
+#include <obd/response/OxygenSensorCodec.hpp>
 
 namespace carpi::obd {
     std::map<std::string, ObdCommand> COMMAND_LIST{};
@@ -21,21 +24,36 @@ namespace carpi::obd {
         using namespace response;
         std::call_once(_command_list_call_flag, []() {
             COMMAND_LIST = {
-                    {"PID01",              {1, "00", EcuType::ENGINE, std::make_shared<BitArrayCodec>(32)}},
-                    {"DTC STATUS",         {1, "01", EcuType::ENGINE, std::make_shared<BitArrayCodec>(32)}},
-                    {"FUEL STATUS",        {1, "03", EcuType::ENGINE, std::make_shared<BitArrayCodec>(16)}},
-                    {"ENGINE LOAD",        {1, "04", EcuType::ENGINE, std::make_shared<EngineLoadCodec>()}},
+                    {"PID01", {1, "00", EcuType::ENGINE, std::make_shared<BitArrayCodec>(32)}},
+                    {"DTC STATUS", {1, "01", EcuType::ENGINE, std::make_shared<BitArrayCodec>(32)}},
+                    {"FUEL STATUS", {1, "03", EcuType::ENGINE, std::make_shared<BitArrayCodec>(16)}},
+                    {"ENGINE LOAD", {1, "04", EcuType::ENGINE, std::make_shared<EngineLoadCodec>()}},
                     {"ENGINE TEMPERATURE", {1, "05", EcuType::ENGINE, std::make_shared<Base40TemperatureCodec>()}},
-                    {"SHORT FUEL TRIM 1",  {1, "06", EcuType::ENGINE, std::make_shared<FuelTrimCodec>()}},
-                    {"LONG FUEL TRIM 1",   {1, "07", EcuType::ENGINE, std::make_shared<FuelTrimCodec>()}},
-                    {"SHORT FUEL TRIM 2",  {1, "08", EcuType::ENGINE, std::make_shared<FuelTrimCodec>()}},
-                    {"LONG FUEL TRIM 2",   {1, "09", EcuType::ENGINE, std::make_shared<FuelTrimCodec>()}},
-                    {"FUEL PRESSURE",      {1, "0A", EcuType::ENGINE, std::make_shared<FuelPressureCodec>()}},
-                    {"INTAKE PRESSURE",    {1, "0B", EcuType::ENGINE, std::make_shared<IntakeManifoldPressureCodec>()}},
-                    {"RPM",                {1, "0C", EcuType::ENGINE, std::make_shared<EngineRpmCodec>()}},
-                    {"SPEED",              {1, "0D", EcuType::ENGINE, std::make_shared<SpeedCodec>()}},
-                    {"TIMING ADVANCE",     {1, "0E", EcuType::ENGINE, std::make_shared<TimingAdvanceCodec>()}},
-
+                    {"SHORT FUEL TRIM 1", {1, "06", EcuType::ENGINE, std::make_shared<FuelTrimCodec>()}},
+                    {"LONG FUEL TRIM 1", {1, "07", EcuType::ENGINE, std::make_shared<FuelTrimCodec>()}},
+                    {"SHORT FUEL TRIM 2", {1, "08", EcuType::ENGINE, std::make_shared<FuelTrimCodec>()}},
+                    {"LONG FUEL TRIM 2", {1, "09", EcuType::ENGINE, std::make_shared<FuelTrimCodec>()}},
+                    {"FUEL PRESSURE", {1, "0A", EcuType::ENGINE, std::make_shared<FuelPressureCodec>()}},
+                    {"INTAKE PRESSURE", {1, "0B", EcuType::ENGINE, std::make_shared<IntakeManifoldPressureCodec>()}},
+                    {"RPM", {1, "0C", EcuType::ENGINE, std::make_shared<EngineRpmCodec>()}},
+                    {"SPEED", {1, "0D", EcuType::ENGINE, std::make_shared<SpeedCodec>()}},
+                    {"TIMING ADVANCE", {1, "0E", EcuType::ENGINE, std::make_shared<TimingAdvanceCodec>()}},
+                    {"INTAKE AIR TEMP", {1, "0F", EcuType::ENGINE, std::make_shared<Base40TemperatureCodec>()}},
+                    {"MAF AIR FLOW", {1, "10", EcuType::ENGINE, std::make_shared<AirflowRateCodec>()}},
+                    {"THROTTLE POSITION", {1, "11", EcuType::ENGINE, std::make_shared<ThrottlePositionCodec>()}},
+                    {"AIR STATUS", {1, "12", EcuType::ENGINE, std::make_shared<BitArrayCodec>(8)}},
+                    {"OXYGEN SENSORS", {1, "13", EcuType::ENGINE, std::make_shared<BitArrayCodec>(8)}},
+                    {"OXYGEN SENSOR 1", {1, "14", EcuType::ENGINE, std::make_shared<OxygenSensorCodec>()}},
+                    {"OXYGEN SENSOR 2", {1, "15", EcuType::ENGINE, std::make_shared<OxygenSensorCodec>()}},
+                    {"OXYGEN SENSOR 3", {1, "16", EcuType::ENGINE, std::make_shared<OxygenSensorCodec>()}},
+                    {"OXYGEN SENSOR 4", {1, "17", EcuType::ENGINE, std::make_shared<OxygenSensorCodec>()}},
+                    {"OXYGEN SENSOR 5", {1, "18", EcuType::ENGINE, std::make_shared<OxygenSensorCodec>()}},
+                    {"OXYGEN SENSOR 6", {1, "19", EcuType::ENGINE, std::make_shared<OxygenSensorCodec>()}},
+                    {"OXYGEN SENSOR 7", {1, "1A", EcuType::ENGINE, std::make_shared<OxygenSensorCodec>()}},
+                    {"OXYGEN SENSOR 8", {1, "1B", EcuType::ENGINE, std::make_shared<OxygenSensorCodec>()}},
+                    {"OBD STANDARDS", {1, "1C", EcuType::ENGINE, std::make_shared<BitArrayCodec>(8)}},
+                    {"OXYGEN SENSORS 2", {1, "1D", EcuType::ENGINE, std::make_shared<BitArrayCodec>(8)}},
+                    {"AUX INPUT STATUS", {1, "1E", EcuType::ENGINE, std::make_shared<BitArrayCodec>(8)}},
                     {"CUR ENGINE RUNTIME", {1, "1F", EcuType::ENGINE, std::make_shared<EngineRuntimeCodec>()}}
             };
         });
