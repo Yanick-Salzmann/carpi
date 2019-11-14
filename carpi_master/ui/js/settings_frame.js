e31a27743fa47256504e8484f6ed8a98a5abec68
yanick.salzmann <yanick.salzmann@gmail.com>
2019-11-14 19:54:21 +0100

    UI stuff

--- carpi_master/ui/js/settings_frame.js
+++ carpi_master/ui/js/settings_frame.js
@@ -11,4 +11,24 @@ $(() => {
     if (env_config['TOMTOM_API_KEY']) {
         $('#tomtom-api-key').val(env_config['TOMTOM_API_KEY']);
     }
+
+    function scan_devices() {
+        event_manager.submitTask('bluetooth_search').then((resp) => {
+            const deck = $('#bluetooth-device-deck');
+            deck.empty();
+
+            resp.forEach(dev => {
+                const card = $('<div class="card"></div>');
+                const header = $('<div class="card-header"></div>');
+                header.text(dev.name);
+                const body = $('<div class="card-body"><p class="card-text"></p></div>');
+                body.find('p').text(dev.address);
+                card.append(header);
+                card.append(body);
+                card.appendTo(deck);
+            })
+        })
+    }
+
+    scan_devices();
 });
