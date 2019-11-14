480adac013933169999642d4c57428bc9a1240c1
yanick.salzmann <yanick.salzmann@gmail.com>
2019-11-14 18:40:48 +0100

    Working on UI

--- carpi_master/ui/js/settings_frame.js
+++ carpi_master/ui/js/settings_frame.js
@@ -1,13 +1,3 @@
 $(() => {
-    const settings = $('section.settings-section > ul > li');
 
-    settings.click((e) => {
-        const li_elem = $(e.target).parents('li');
-        if (li_elem.hasClass('expanded')) {
-            return;
-        }
-
-        settings.removeClass('expanded');
-        li_elem.addClass('expanded');
-    });
 });
