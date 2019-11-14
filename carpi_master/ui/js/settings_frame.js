c57d8b2baac8dae741bcc28eb88949f14886ecfd
yanick.salzmann <yanick.salzmann@gmail.com>
2019-11-14 19:44:57 +0100

    UI stuff

--- carpi_master/ui/js/settings_frame.js
+++ carpi_master/ui/js/settings_frame.js
@@ -1,10 +1,14 @@
 $(() => {
-    const here_config = get_env_value('HERE_APP_ID', 'HERE_APP_CODE');
-    if (here_config['HERE_APP_ID']) {
-        $('#here-api-app').val(here_config['HERE_APP_ID']);
+    const env_config = get_env_value('HERE_APP_ID', 'HERE_APP_CODE', 'TOMTOM_API_KEY');
+    if (env_config['HERE_APP_ID']) {
+        $('#here-api-app').val(env_config['HERE_APP_ID']);
     }
 
-    if (here_config['HERE_APP_CODE']) {
-        $('#here-api-code').val(here_config['HERE_APP_CODE']);
+    if (env_config['HERE_APP_CODE']) {
+        $('#here-api-code').val(env_config['HERE_APP_CODE']);
+    }
+
+    if (env_config['TOMTOM_API_KEY']) {
+        $('#tomtom-api-key').val(env_config['TOMTOM_API_KEY']);
     }
 });
