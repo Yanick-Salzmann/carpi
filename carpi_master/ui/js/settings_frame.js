52960a2a18a2890820e8f5831fcb8a7ac484e27b
yanick.salzmann <yanick.salzmann@gmail.com>
2019-11-14 18:43:08 +0100

    Working on UI

--- carpi_master/ui/js/settings_frame.js
+++ carpi_master/ui/js/settings_frame.js
@@ -1,3 +1,10 @@
 $(() => {
+    const here_config = get_env_value('HERE_APP_ID', 'HERE_APP_CODE');
+    if (here_config['HERE_APP_ID']) {
+        $('#here-api-app').val(here_config['HERE_APP_ID']);
+    }
 
+    if (here_config['HERE_APP_CODE']) {
+        $('#here-api-code').val(here_config['HERE_APP_CODE']);
+    }
 });
