86c76958b73e2612a0b73c41bab481f1666b6eff
yanick.salzmann <yanick.salzmann@gmail.com>
2019-11-16 10:12:37 +0100

    UI stuff

---
+++ carpi_master/ui/js/gauge.js
@@ -0,0 +1,20 @@
+(() => {
+    function draw_gauge(elem) {
+        const ctx = elem.getContext('2d');
+        ctx.shadowBlur = 5;
+        ctx.shadowColor = 'white';
+        ctx.fillStyle = "white";
+        ctx.strokeStyle = "white";
+        ctx.lineWidth = 1;
+
+        ctx.beginPath();
+        ctx.arc(150, 150, 120, 3 * Math.PI / 4, Math.PI/4);
+        ctx.stroke();
+        ctx.arc(150, 150, 130, Math.PI/4, 3 * Math.PI / 4, true);
+        ctx.stroke();
+        ctx.arc(150, 150, 120, 3 * Math.PI / 4, 3 * Math.PI / 4);
+        ctx.stroke();
+    }
+
+    $('canvas.gauge').forEach(cvs => draw_gauge(cvs));
+});
