968866c00caf64bb2d7f6a3dbb027597c5f1cd02
yanick.salzmann <yanick.salzmann@gmail.com>
2019-11-16 12:19:50 +0100

    UI stuff

--- carpi_master/ui/js/gauge.js
+++ carpi_master/ui/js/gauge.js
@@ -2,8 +2,6 @@ $(() => {
     function draw_gauge(elem) {
         const ctx = elem.getContext('2d');
         const origin = 150;
-        const inner = 120;
-        const outer = 135;
 
         ctx.shadowBlur = 10;
         ctx.shadowColor = 'white';
@@ -12,25 +10,41 @@ $(() => {
         ctx.lineWidth = 2;
 
         ctx.beginPath();
-        ctx.arc(origin, origin, inner, 3 * Math.PI / 4, Math.PI/4);
+        ctx.arc(origin, origin, 120, 3 * Math.PI / 4, Math.PI/4);
         ctx.stroke();
-        ctx.arc(origin, origin, outer, Math.PI/4, 3 * Math.PI / 4, true);
+        ctx.arc(origin, origin, 135, Math.PI/4, 3 * Math.PI / 4, true);
         ctx.stroke();
-        ctx.arc(origin, origin, inner, 3 * Math.PI / 4, 3 * Math.PI / 4);
+        ctx.arc(origin, origin, 120, 3 * Math.PI / 4, 3 * Math.PI / 4);
         ctx.stroke();
 
-        for(let i = 0; i < 7; ++i) {
+        const xofs = [0,   0,    0.3, -0.3, -0.7, -1.1,   -1.2,  -1.2];
+        const yofs = [0.4, 0.45, 0.6,  0.7,  0.7, 0.45, 0.3, 0.4];
+        const texts = [ '0', '1', '2', '3', '4', '5', '6', '7'];
+
+        for(let i = 0; i < 8; ++i) {
             const grad = ((3 * Math.PI) / 14) * i + (3 * Math.PI / 4);
             ctx.beginPath();
-            const sx = origin + inner * Math.cos(grad);
-            const sy = origin + inner * Math.sin(grad);
+            const sx = origin + 120 * Math.cos(grad);
+            const sy = origin + 120 * Math.sin(grad);
 
-            const dx = origin + outer * Math.cos(grad);
-            const dy = origin + outer * Math.sin(grad);
+            const dx = origin + 135 * Math.cos(grad);
+            const dy = origin + 135 * Math.sin(grad);
 
             ctx.moveTo(sx, sy);
             ctx.lineTo(dx, dy);
             ctx.stroke();
+
+            const textSize = 15;
+
+            ctx.beginPath();
+            ctx.font = textSize + 'px sans-serif'
+            const text = texts[i];
+            const dimension = ctx.measureText(text);
+
+            const tx = origin + 110 * Math.cos(grad) + xofs[i] * dimension.width;
+            const ty = origin + 110 * Math.sin(grad) + yofs[i] * textSize;
+
+            ctx.fillText(text, tx, ty);
         }
     }
 
