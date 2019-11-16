871d0e9c8d1983b166032fa2b427b5fcab1fddff
yanick.salzmann <yanick.salzmann@gmail.com>
2019-11-16 14:10:38 +0100

    UI stuff

--- carpi_master/ui/js/car_frame.js
+++ carpi_master/ui/js/car_frame.js
@@ -1,4 +1,6 @@
 $(() => {
+    const canvas_origin = 150;
+
     function draw_pin_root(ctx, origin) {
         ctx.beginPath();
         ctx.shadowBlur = 10;
@@ -91,14 +93,14 @@ $(() => {
         ctx.stroke();
     }
 
-    function draw_rpm_gauge(ctx) {
-        const origin = 150;
+    function draw_rpm_gauge(ctx, rpm) {
+        ctx.clearRect(0, 0, 300, 300);
         const xofs = [0, 0, 0.3, -0.3, -0.7, -1.1, -1.2, -1.2];
         const yofs = [0.4, 0.45, 0.6, 0.7, 0.7, 0.45, 0.45, 0.4];
         const texts = ['0', '1', '2', '3', '4', '5', '6', '7'];
 
-        draw_gauge_shape(ctx, origin);
-        draw_sub_segments(ctx, origin, 70, index => {
+        draw_gauge_shape(ctx, canvas_origin);
+        draw_sub_segments(ctx, canvas_origin, 70, index => {
             if ((index % 10) === 0) {
                 return 1;
             } else if ((index % 5) === 0) {
@@ -107,19 +109,19 @@ $(() => {
                 return 0.2;
             }
         });
-        draw_indicators(ctx, origin, xofs, yofs, texts);
-        draw_pin_root(ctx, origin);
-        draw_pin(ctx, origin, 2000, 7000);
+        draw_indicators(ctx, canvas_origin, xofs, yofs, texts);
+        draw_pin_root(ctx, canvas_origin);
+        draw_pin(ctx, canvas_origin, rpm, 7000);
     }
 
-    function draw_speed_gauge(ctx) {
-        const origin = 150;
+    function draw_speed_gauge(ctx, speed) {
+        ctx.clearRect(0, 0, 300, 300);
         const xofs = [0.4, 0.3, 0.3, 0.3, 0.0, -0.5, -0.6, -1.0, -1.2, -1.2, -1.2, -1.2];
         const yofs = [0.4, 0.45, 0.4, 0.7, 0.7, 0.95, 0.95, 0.8, 0.6, 0.3, 0.2, 0.4];
         const texts = ['0', '20', '40', '60', '80', '100', '120', '140', '160', '180', '200', '220'];
 
-        draw_gauge_shape(ctx, origin);
-        draw_sub_segments(ctx, origin, 110, index => {
+        draw_gauge_shape(ctx, canvas_origin);
+        draw_sub_segments(ctx, canvas_origin, 110, index => {
             if ((index % 10) === 0) {
                 return 1;
             } else if ((index % 5) === 0) {
@@ -128,16 +130,70 @@ $(() => {
                 return 0.2;
             }
         });
-        draw_indicators(ctx, origin, xofs, yofs, texts);
-        draw_pin_root(ctx, origin);
-        draw_pin(ctx, origin, 0, 220);
+        draw_indicators(ctx, canvas_origin, xofs, yofs, texts);
+        draw_pin_root(ctx, canvas_origin);
+        draw_pin(ctx, canvas_origin, speed, 220);
+
+        ctx.beginPath();
     }
 
-    window.on_show_car_section = () => {
-        const rpm_gauge = $('#rpm-gauge')[0];
-        const speed_gauge = $('#speed-gauge')[0];
+    const rpm_gauge = $('#rpm-gauge')[0];
+    const speed_gauge = $('#speed-gauge')[0];
 
-        draw_rpm_gauge(rpm_gauge.getContext('2d'));
-        draw_speed_gauge(speed_gauge.getContext('2d'));
+    const ctx_rpm = rpm_gauge.getContext('2d');
+    const ctx_speed = speed_gauge.getContext('2d');
+
+    function update_rpm(rpm) {
+        draw_rpm_gauge(ctx_rpm, Math.min(Math.max(0, rpm), 7000));
+    }
+
+    function update_speed(speed) {
+        draw_speed_gauge(ctx_speed, Math.min(Math.max(0, speed), 220));
+        const text = (speed.toFixed(2));
+        ctx_speed.beginPath();
+        const dimension = ctx_speed.measureText(text);
+        const dim_static = ctx_speed.measureText("km/h");
+        ctx_speed.fillStyle = 'white';
+        ctx_speed.shadowColor = 'white';
+        ctx_speed.fillText(text, 150 - dimension.width / 2.0, 240);
+        ctx_speed.fillText("km/h", 150 - dim_static.width / 2.0, 260);
+    }
+
+    let last_interval = null;
+
+    const times  = [0, 1000, 1500, 1800, 2000, 2400, 2700, 2800, 3200, 3500, 4000];
+    const values = [0, 1800, 2400, 2300, 2100, 1500, 1400,  1300,  1500, 750, 0];
+
+    window.on_show_car_section = () => {
+        if(last_interval) {
+            clearInterval(last_interval);
+        }
+
+        update_rpm(0);
+        update_speed(0);
+
+        let start_time = (new Date()).getTime();
+
+        setInterval(() => {
+            const diff = ((new Date()).getTime() - start_time) % 4000;
+            let index = times.length - 2;
+
+            for(let i = 0; i < (times.length - 1); ++i) {
+                if(times[i] <= diff && times[i + 1] >= diff) {
+                    index = i;
+                    break;
+                }
+            }
+
+            const start = times[index];
+            const end = times[index + 1];
+
+            const v0 = values[index];
+            const v1 = values[index + 1];
+
+            const fac = (diff - start) / (end - start);
+
+            update_rpm(v0 + fac * (v1 - v0));
+        }, 10);
     }
 });
