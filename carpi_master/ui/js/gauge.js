a4f61547db4f9353074ae84083ceb9ad129530dd
yanick.salzmann <yanick.salzmann@gmail.com>
2019-11-16 11:37:22 +0100

    UI stuff

--- carpi_master/ui/js/gauge.js
+++ carpi_master/ui/js/gauge.js
@@ -1,16 +1,16 @@
 $(() => {
     function draw_gauge(elem) {
         const ctx = elem.getContext('2d');
-        ctx.shadowBlur = 5;
+        ctx.shadowBlur = 10;
         ctx.shadowColor = 'white';
         ctx.fillStyle = "white";
         ctx.strokeStyle = "white";
-        ctx.lineWidth = 1;
+        ctx.lineWidth = 2;
 
         ctx.beginPath();
         ctx.arc(150, 150, 120, 3 * Math.PI / 4, Math.PI/4);
         ctx.stroke();
-        ctx.arc(150, 150, 130, Math.PI/4, 3 * Math.PI / 4, true);
+        ctx.arc(150, 150, 135, Math.PI/4, 3 * Math.PI / 4, true);
         ctx.stroke();
         ctx.arc(150, 150, 120, 3 * Math.PI / 4, 3 * Math.PI / 4);
         ctx.stroke();
