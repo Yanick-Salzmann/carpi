f16090e3a855994377ffd617f71bfe3361cf7b8a
yanick.salzmann <yanick.salzmann@gmail.com>
2019-11-17 20:11:24 +0100

    MQ testing

--- gps_broadcast/src/net_broadcast.cpp
+++ gps_broadcast/src/net_broadcast.cpp
@@ -21,7 +21,8 @@ namespace carpi::gps {
             freeifaddrs(addrs);
         });
 
-        std::string net_mask = inet_ntoa(((const sockaddr_in*) if_addrs->ifa_addr)->sin_addr);
+        char ipv6_addr[1024]{};
+        std::string net_mask = inet_ntop(if_addrs->ifa_addr->sa_family, if_addrs->ifa_addr, ipv6_addr, sizeof ipv6_addr);
         log->info("Net Mask: {}", net_mask);
     }
 }
