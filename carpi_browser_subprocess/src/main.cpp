b3c0645783ded1ee0960342eafc4c601dbbeaa2e
yanick.salzmann <yanick.salzmann@gmail.com>
2019-11-17 19:03:14 +0100

    MQ testing

--- carpi_browser_subprocess/src/main.cpp
+++ carpi_browser_subprocess/src/main.cpp
@@ -7,6 +7,6 @@
 int main(int argc, char* argv[]) {
     CefMainArgs main_args{argc, argv};
     CefRefPtr<carpi::MainApp> app{new carpi::MainApp{}};
-    sGpsEvtLstnr->run();
+    //sGpsEvtLstnr->run();
     return CefExecuteProcess(main_args, app, nullptr);
 }
