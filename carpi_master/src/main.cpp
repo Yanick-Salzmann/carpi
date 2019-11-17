aa225324206ace4cc512dd5de499d664f84cf315
yanick.salzmann <yanick.salzmann@gmail.com>
2019-11-17 18:59:54 +0100

    MQ testing

--- carpi_master/src/main.cpp
+++ carpi_master/src/main.cpp
@@ -4,12 +4,25 @@
 #include <ui/web_core.hpp>
 #include <data_server/http_server.hpp>
 #include <io/camera/camera_stream.hpp>
+#include <mqueue.h>
 
 namespace carpi {
     int _argc;
     char **_argv;
 
+    void mq_listener() {
+        mq_attr attributes{
+                .mq_maxmsg = 5,
+                .mq_msgsize = 512
+        };
+        const auto mq = mq_open("/gpsbroadcast", O_RDWR | O_CREAT | O_CLOEXEC, 0777, &attributes);
+        std::array<std::uint8_t, 512> message_data{};
+        const auto msg_size = mq_receive(mq, (char *) message_data.data(),512, nullptr);
+        std::cout << "Read: " << msg_size << std::endl;
+    }
+
     int main(int argc, char *argv[]) {
+        std::thread t{mq_listener};
         std::string line{};
         utils::Logger log{"main"};
         _argc = argc;
