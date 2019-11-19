f518a152d48940a890aed65515173dfaada4aa70
yanick.salzmann <yanick.salzmann@gmail.com>
2019-11-19 19:53:34 +0100

    Using UDP multicast

--- net_utils/src/udp_multicast.cpp
+++ net_utils/src/udp_multicast.cpp
@@ -7,9 +7,9 @@ namespace carpi::net {
     LOGGER_IMPL(UdpMulticast);
 
     UdpMulticast::UdpMulticast(const std::string &address, uint16_t port, bool receiver) {
-        if(inet_pton(AF_INET6, address.c_str(), &_addr6) == 1) {
+        if (inet_pton(AF_INET6, address.c_str(), &_addr6) == 1) {
             _addr_family = AF_INET6;
-        } else if(inet_pton(AF_INET, address.c_str(), &_addr4) == 1) {
+        } else if (inet_pton(AF_INET, address.c_str(), &_addr4) == 1) {
             _addr_family = AF_INET;
         } else {
             log->error("Cannot parse '{}' as IPv4 or IPv6 address");
@@ -17,13 +17,13 @@ namespace carpi::net {
         }
 
         _socket = socket(_addr_family, SOCK_DGRAM, 0);
-        if(_socket < 0) {
+        if (_socket < 0) {
             log->error("Error creating UDP socket: {} (errno={})", utils::error_to_string(errno), errno);
             throw std::runtime_error{"Error creating UDP socket"};
         }
 
         uint32_t reuse = 0;
-        if(setsockopt(_socket, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof reuse) < 0) {
+        if (setsockopt(_socket, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof reuse) < 0) {
             log->error("Error setting SO_REUSEADDR on UDP socket: {} (errno={})", utils::error_to_string(errno), errno);
             throw std::runtime_error{"Error creating UDP socket"};
         }
@@ -31,7 +31,7 @@ namespace carpi::net {
         _addr4.sin_port = htons(port);
         _addr6.sin6_port = htons(port);
 
-        if(receiver) {
+        if (receiver) {
             const auto inaddr4 = _addr4.sin_addr;
             const auto inaddr6 = _addr6.sin6_addr;
 
@@ -41,34 +41,34 @@ namespace carpi::net {
         }
     }
 
-    void UdpMulticast::bind_receiver(const in_addr& gaddr4, const in6_addr& gaddr6) {
-        if(_addr_family == AF_INET) {
-            if(bind(_socket, (const sockaddr*) &_addr4, sizeof _addr4) < 0) {
+    void UdpMulticast::bind_receiver(const in_addr &gaddr4, const in6_addr &gaddr6) {
+        if (_addr_family == AF_INET) {
+            if (bind(_socket, (const sockaddr *) &_addr4, sizeof _addr4) < 0) {
                 log->error("Error binding UDP socket: {} (errno={})", utils::error_to_string(errno), errno);
                 throw std::runtime_error{"Error binding UDP"};
             }
 
             ip_mreq mreq{
-                .imr_interface.s_addr = htonl(INADDR_ANY),
-                .imr_multiaddr = gaddr4
+                    .imr_multiaddr = gaddr4,
+                    .imr_interface.s_addr = htonl(INADDR_ANY)
             };
 
-            if(setsockopt(_socket, IPPROTO_IP, IP_ADD_MEMBERSHIP, &mreq, sizeof mreq) < 0) {
+            if (setsockopt(_socket, IPPROTO_IP, IP_ADD_MEMBERSHIP, &mreq, sizeof mreq) < 0) {
                 log->error("Error adding membership to multicast address: {} (errno={})", utils::error_to_string(errno), errno);
                 throw std::runtime_error{"Error joining multicast"};
             }
         } else {
-            if(bind(_socket, (const sockaddr*) &_addr6, sizeof _addr6) < 0) {
+            if (bind(_socket, (const sockaddr *) &_addr6, sizeof _addr6) < 0) {
                 log->error("Error binding UDP socket: {} (errno={})", utils::error_to_string(errno), errno);
                 throw std::runtime_error{"Error binding UDP"};
             }
 
-            ipv6_mreq mreq {
-                .ipv6mr_interface = 0,
-                .ipv6mr_multiaddr = gaddr6
+            ipv6_mreq mreq{
+                    .ipv6mr_multiaddr = gaddr6,
+                    .ipv6mr_interface = 0
             };
 
-            if(setsockopt(_socket, IPPROTO_IPV6, IP_ADD_MEMBERSHIP, &mreq, sizeof mreq) < 0) {
+            if (setsockopt(_socket, IPPROTO_IPV6, IP_ADD_MEMBERSHIP, &mreq, sizeof mreq) < 0) {
                 log->error("Error adding membership to multicast address: {} (errno={})", utils::error_to_string(errno), errno);
                 throw std::runtime_error{"Error joining multicast"};
             }
@@ -76,10 +76,10 @@ namespace carpi::net {
     }
 
     std::ptrdiff_t UdpMulticast::send_data(const std::vector<uint8_t> &data) {
-        if(_addr_family == AF_INET) {
-            return sendto(_socket, data.data(), data.size(), 0, (const sockaddr*) &_addr4, sizeof _addr4);
+        if (_addr_family == AF_INET) {
+            return sendto(_socket, data.data(), data.size(), 0, (const sockaddr *) &_addr4, sizeof _addr4);
         } else {
-            return sendto(_socket, data.data(), data.size(), 0, (const sockaddr*) &_addr6, sizeof _addr6);
+            return sendto(_socket, data.data(), data.size(), 0, (const sockaddr *) &_addr6, sizeof _addr6);
         }
     }
 
@@ -88,14 +88,14 @@ namespace carpi::net {
     }
 
     std::ptrdiff_t UdpMulticast::read_data(void *buffer, std::size_t to_read) {
-        if(_addr_family == AF_INET) {
+        if (_addr_family == AF_INET) {
             sockaddr_in remote_addr{};
             socklen_t remote_len = sizeof remote_addr;
-            return recvfrom(_socket, buffer, to_read, 0, (sockaddr*)&remote_addr, &remote_len);
+            return recvfrom(_socket, buffer, to_read, 0, (sockaddr *) &remote_addr, &remote_len);
         } else {
             sockaddr_in6 remote_addr{};
             socklen_t remote_len = sizeof remote_addr;
-            return recvfrom(_socket,buffer, to_read, 0, (sockaddr*)&remote_addr, &remote_len);
+            return recvfrom(_socket, buffer, to_read, 0, (sockaddr *) &remote_addr, &remote_len);
         }
     }
 }
