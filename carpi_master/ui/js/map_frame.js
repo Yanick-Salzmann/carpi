f00ee51d47cb8e7eb424575636011e0b2eb04218
yanick.salzmann <yanick.salzmann@gmail.com>
2019-11-20 18:47:18 +0100

    Working on here map

--- carpi_master/ui/js/map_frame.js
+++ carpi_master/ui/js/map_frame.js
@@ -2,11 +2,18 @@ $(() => {
     const env_config = get_env_value('HERE_APP_ID', 'HERE_APP_CODE');
 
     const map = L.map('leaflet-map-container');
+    const cur_pos = L.circle([0,0], {
+        color: 'blue',
+            fillColor: '#30f',
+            fillOpacity: 0.5,
+            radius: 2
+    }).addTo(map);
     let is_initialized = false;
 
     function on_update() {
         const position = gps_get_coordinates();
         map.setView([position.lat, position.lon], 21);
+        cur_pos.setLatLng([position.lat, position.lon]);
     }
 
     window.on_show_map_section = function () {
