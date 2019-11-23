194ff89a1ac48bbe11367c1bf90cb399d096d72a
yanick.salzmann <yanick.salzmann@gmail.com>
2019-11-23 12:33:00 +0100

    Trying to get traffic overlay

--- carpi_master/ui/js/map_frame.js
+++ carpi_master/ui/js/map_frame.js
@@ -82,7 +82,7 @@ $(() => {
         if (!is_initialized) {
             is_initialized = true;
 
-            L.tileLayer.here({appId: env_config['HERE_APP_ID'], appCode: env_config['HERE_APP_CODE'], scheme: 'hybrid.traffic.day'})
+            L.tileLayer.here({appId: env_config['HERE_APP_ID'], appCode: env_config['HERE_APP_CODE'], scheme: 'hybrid.day', resource: 'traffictile'})
                 .addTo(map);
 
             gps_get_coordinates();
