bfdd0c75d724c41cc2e4a0989a2639c9b51ea00d
yanick.salzmann <yanick.salzmann@gmail.com>
2019-11-23 13:38:18 +0100

    Getting PLZ from switzerland

--- carpi_master/ui/js/csv_reader.js
+++ carpi_master/ui/js/csv_reader.js
@@ -1,17 +1,23 @@
 class CsvReader {
     constructor(path) {
+        this.rows = [];
+
         this.promise = new Promise((resolve, reject) => {
             $.get(path).done(data => {
-                console.log(data);
+                this.process_lines(data);
+                resolve(this.rows);
             }).fail(() => {
                 reject();
             })
         });
-
     }
-
 
     then(success, error) {
         this.promise.then(success, error);
+    }
+
+    process_lines(data) {
+        const lines = data.split(/$/m);
+        console.log(lines.length);
     }
 }
