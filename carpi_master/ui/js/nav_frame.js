54edb336e901444bbf2037e978eaeacf1171cadc
yanick.salzmann <yanick.salzmann@gmail.com>
2019-11-24 16:20:48 +0100

    Working with CH_addresses.csv

--- carpi_master/ui/js/nav_frame.js
+++ carpi_master/ui/js/nav_frame.js
@@ -33,13 +33,13 @@ $(() => {
         $('#ch-post-code-input-target').val(plz_prefix);
         const container = $('#nav-wizard-step-addr-ch .item-recommendation');
         const len = plz_prefix.length;
-        console.log(len);
         if (len > 4) {
             container.empty();
             return;
         }
 
         if (!len) {
+            container.empty();
             loadInitialPlzValues();
             return;
         }
