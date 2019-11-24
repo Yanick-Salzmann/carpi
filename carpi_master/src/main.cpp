8ee4e09eb68c485baf6077b21f018bc3fecd0960
yanick.salzmann <yanick.salzmann@gmail.com>
2019-11-24 15:51:17 +0100

    Working with CH_addresses.csv

--- carpi_master/src/main.cpp
+++ carpi_master/src/main.cpp
@@ -14,7 +14,7 @@ namespace carpi {
         utils::Logger log{"main"};
 
         wiring::Gpio gpio;
-        auto pin = gpio.open_pin(21);
+        auto pin = gpio.open_pin(40);
         pin.mode(wiring::GpioMode::OUTPUT_MODE);
         while(true) {
             pin.high();
