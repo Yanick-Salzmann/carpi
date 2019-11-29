0b55831fcb5ea6aefb9c73f21644d45478238e78
yanick.salzmann <yanick.salzmann@gmail.com>
2019-11-29 06:16:08 +0100

    Nav frame stuff

--- carpi_master/ui/js/nav_frame.js
+++ carpi_master/ui/js/nav_frame.js
@@ -68,7 +68,7 @@ $(() => {
 
     function updateStreetNumberKeyboard(addresses) {
         const available = addresses.filter(addr => addr.number.length > number_prefix.length)
-            .map(addr => addr.number[number_prefix].toLowerCase());
+            .map(addr => addr.number[number_prefix.length].toLowerCase());
 
         street_num_keyboard.filterEnabledKeys(key => {
             return key === '\b' && available.indexOf(key) >= 0;
