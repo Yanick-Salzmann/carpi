fb63c1786e8681f51dec3d01a951c6dd5fc4d446
yanick.salzmann <yanick.salzmann@gmail.com>
2019-12-04 20:10:30 +0100

    Serial interface logic

---
+++ wiring_utils/include/wiring_utils/serial_interface.hpp
@@ -0,0 +1,28 @@
+#ifndef CARPI_WIRING_UTILS_SERIAL_INTERFACE_HPP
+#define CARPI_WIRING_UTILS_SERIAL_INTERFACE_HPP
+
+#include <string>
+#include <common_utils/log.hpp>
+#include <chrono>
+
+namespace carpi::wiring {
+    class SerialInterface {
+        LOGGER;
+
+        int _file;
+
+    public:
+        explicit SerialInterface(const std::string& device, uint32_t baud_rate);
+
+        void write(const std::string& data);
+
+        std::string read(std::chrono::nanoseconds timeout_ns);
+
+        template<typename Rep, typename Period>
+        std::string read(std::chrono::duration<Rep, Period> timeout) {
+            return read(std::chrono::duration_cast<std::chrono::nanoseconds>(timeout));
+        }
+    };
+}
+
+#endif //CARPI_WIRING_UTILS_SERIAL_INTERFACE_HPP
---
+++ wiring_utils/src/SerialInterface.cpp
@@ -0,0 +1,55 @@
+#include <wiring_utils/serial_interface.hpp>
+#include <wiringSerial.h>
+#include <common_utils/error.hpp>
+
+namespace carpi::wiring {
+    LOGGER_IMPL(SerialInterface);
+
+    SerialInterface::SerialInterface(const std::string &device, uint32_t baud_rate) {
+        _file = serialOpen(device.c_str(), baud_rate);
+        if (_file < 0) {
+            log->error("Error opening serial interface {}: {} (errno={})", device, utils::error_to_string(errno), errno);
+            throw std::runtime_error{"Error opening serial interface"};
+        }
+    }
+
+    void SerialInterface::write(const std::string &data) {
+        serialPuts(_file, data.c_str());
+    }
+
+    std::string SerialInterface::read(std::chrono::nanoseconds timeout_ns) {
+        const auto secs = std::chrono::duration_cast<std::chrono::seconds>(timeout_ns);
+        timeout_ns -= secs;
+
+        timespec sleep_val{
+                .tv_sec = secs.count(),
+                .tv_nsec = timeout_ns.count()
+        };
+
+        auto has_read = false;
+        std::string ret_val{};
+        int next_char;
+        do {
+            const auto num_available = serialDataAvail(_file);
+            if (!num_available) {
+                const auto result = nanosleep(&sleep_val, nullptr);
+                if (result != 0 && result != EINTR) {
+                    log->error("Error sleeping: {} (errno={})", utils::error_to_string(errno), errno);
+                    throw std::runtime_error{"Error reading from serial interface"};
+                }
+            }
+
+            if (num_available < 0) {
+                return ret_val;
+            }
+
+            next_char = serialGetchar(_file);
+            if (next_char != -1) {
+                ret_val += (char) next_char;
+            }
+        } while (next_char != -1);
+
+        return ret_val;
+    }
+
+}
